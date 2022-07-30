fc7a34b79fafae6bdfc9f627e2b0607d0d30d0eb
PGG106 <g.pagano45@studenti.unisa.it>
2022-07-30 13:10:27 +0200

    change time management for the moves to go format

--- src/time_manager.cpp
+++ src/time_manager.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <cfloat>
 #include <cmath>
-
+#define BASE_DIV 20
 //Calculate how much time to spend on searching a move
 int optimum(S_Board* pos, S_SearchINFO* info, int time, int inc) {
 
@@ -14,12 +14,9 @@ int optimum(S_Board* pos, S_SearchINFO* info, int time, int inc) {
 
 		info->timeset = TRUE;
 		//If we recieved a movestogo parameter
-		if (info->movestogo != -1) {
-			//time is equal to (total time)/ number of moves we have to play - a safety overhead
-			time /= info->movestogo;
-			time -= 50;
+		if (info->movestogo != -1 && info->movestogo < BASE_DIV) {
+			time = (std::max)(time / BASE_DIV, time / info->movestogo - 50);
 			return (info->starttime + (time + inc / 2));
-
 		}
 		//if not we just use the time remaining/20
 		else return (info->starttime + (time / 20 + inc / 2));
