cc6044d97376d72811f5a07623268d8eb7e8ba02
PGG106 <g.pagano45@studenti.unisa.it>
2022-08-06 08:39:36 +0200

    change how and when HH is updated

--- src/History.cpp
+++ src/History.cpp
@@ -6,7 +6,7 @@ void updateHH(S_Board* pos, int depth, int bestmove, S_MOVELIST* quiet_moves) {
 	if (depth > 1)
 		//if we are at a depth >1 increment the history score of the best move
 		pos->searchHistory[pos->pieces[get_move_source(bestmove)]]
-		[get_move_target(bestmove)] += depth*depth*depth;
+		[get_move_target(bestmove)] += depth * depth;
 	//Loop through all the quiet moves
 	for (int i = 0; i < quiet_moves->count; i++) {
 		int move = quiet_moves->moves[i].move;
@@ -14,7 +14,7 @@ void updateHH(S_Board* pos, int depth, int bestmove, S_MOVELIST* quiet_moves) {
 			continue;
 		else { // if the move isn't the best move decrease its history score
 			pos->searchHistory[pos->pieces[get_move_source(move)]]
-				[get_move_target(move)] -= depth*depth*depth;
+				[get_move_target(move)] -= depth * depth;
 		}
 	}
 }
