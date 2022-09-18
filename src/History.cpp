9cf2cc8061398cc52a3d7e81dea99af51ff78e30
PGG106 <g.pagano45@studenti.unisa.it>
2022-09-18 16:30:07 +0200

    tweak

--- src/History.cpp
+++ src/History.cpp
@@ -7,10 +7,12 @@
 //Update the history heuristics of all the quiet moves passed to the function
 void updateHH(S_Board* pos, int depth, int bestmove, S_MOVELIST* quiet_moves) {
 	int bonus = depth * depth;
-	bonus = bonus - getHHScore(pos, bestmove) * abs(bonus) / 32768;
+
 	//Loop through all the quiet moves
 	for (int i = 0; i < quiet_moves->count; i++) {
 		int move = quiet_moves->moves[i].move;
+		bonus = bonus - getHHScore(pos, move) * abs(bonus) / 32768;
+
 		if (move == bestmove)
 			//if we are at a depth >1 increment the history score of the best move
 			pos->searchHistory[pos->pieces[get_move_source(bestmove)]]
