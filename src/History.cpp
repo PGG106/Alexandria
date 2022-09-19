bdf476c5c5a2ca6f454b05afa318baf78b6521fe
PGG106 <giuseppepagano10@gmail.com>
2022-09-19 19:08:25 +0200

    Cap history score to +-32768 (#39)
    * put a cap on the total history value a move can get
    
    * fix missing header
    
    * fix  missing header 2
    
    * tweak

--- src/History.cpp
+++ src/History.cpp
@@ -1,12 +1,18 @@
 #include "Board.h"
 #include "move.h"
+#include "History.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 //Update the history heuristics of all the quiet moves passed to the function
 void updateHH(S_Board* pos, int depth, int bestmove, S_MOVELIST* quiet_moves) {
 	int bonus = depth * depth;
+
 	//Loop through all the quiet moves
 	for (int i = 0; i < quiet_moves->count; i++) {
 		int move = quiet_moves->moves[i].move;
+		bonus = bonus - getHHScore(pos, move) * abs(bonus) / 32768;
+
 		if (move == bestmove)
 			//if we are at a depth >1 increment the history score of the best move
 			pos->searchHistory[pos->pieces[get_move_source(bestmove)]]
