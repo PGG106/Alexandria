6ddb24434b83129285661334e9303b55653e9a7d
PGG106 <g.pagano45@studenti.unisa.it>
2022-12-03 14:45:05 +0100

    datagen stub

---
+++ datagen.cpp
@@ -0,0 +1,11 @@
+void datagen()
+{
+	// Play 5 random moves
+
+	//From this point onwards search as normal
+
+	//At the end of search output position and score to a file
+
+	// Once the game ends append the game result to every newly added position
+
+}
