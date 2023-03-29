e1349f53e2b6444b9f06d990f5a4c617aec278eb
PGG106 <g.pagano45@studenti.unisa.it>
2023-03-29 06:22:41 +0200

    Expand material draw
    Bench:: 3539216

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,14 +1,17 @@
 #include "eval.h"
 
 // if we don't have enough material to mate consider the position a draw
-int MaterialDraw(const S_Board* pos) {
+bool MaterialDraw(const S_Board* pos) {
 	//If we only have kings on the board then it's a draw
 	if (CountBits(Occupancy(pos, BOTH)) == 2)
-		return TRUE;
+		return true;
+	//KN v K, KB v K
 	else if (CountBits(Occupancy(pos, BOTH)) == 3 && ((CountBits(GetPieceBB(pos, KNIGHT)) == 1) || (CountBits(GetPieceBB(pos, BISHOP)) == 1)))
-		return TRUE;
-
-	return FALSE;
+		return true;
+	//KNN v K, KN v KN
+	else if (CountBits(Occupancy(pos, BOTH)) == 4 && ((CountBits(GetPieceBB(pos, KNIGHT)) == 2)))
+		return true;
+	return false;
 }
 
 // position evaluation
--- src/eval.h
+++ src/eval.h
@@ -2,6 +2,6 @@
 #include "board.h"
 
 //Returns if the position is a draw because there isn't enough mating material
-int MaterialDraw(const S_Board* pos);
+bool MaterialDraw(const S_Board* pos);
 // position evaluation
 int EvalPosition(const S_Board* pos);
