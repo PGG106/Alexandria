c71ec36ca4f3277c03126d9563135ac6052031a6
kz04px <6536046+kz04px@users.noreply.github.com>
2023-06-28 15:23:59 +0100

    Simplification
    return true/false directly from return value instead of if else

--- src/misc.cpp
+++ src/misc.cpp
@@ -35,12 +35,5 @@ std::vector<std::string> split_command(const std::string& command)
 
 //returns true if in a vector of string there's one that matches the key
 bool Contains(std::vector<std::string> tokens, std::string key) {
-
-	if (std::find(tokens.begin(), tokens.end(), key) != tokens.end()) {
-		return true;
-	}
-	else {
-		return false;
-	}
-	return false;
+	return std::find(tokens.begin(), tokens.end(), key) != tokens.end();
 }
