af043aeaffc6a4ffad71e77cfa0771c54892049a
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-16 08:26:19 +0800

    Update comment

--- src/search.cpp
+++ src/search.cpp
@@ -421,7 +421,9 @@ int Negamax(int alpha, int beta, int depth, const bool cutnode, S_ThreadData* td
 	if (ttHit)
 		ttpv = pv_node || tte.wasPv;
 
-	// IIR by Ed Schroder (That i find out about in Berserk source code)
+	// IIR (Internal iterative reductions)
+	// inspired by Ed Schroder (That I find out about in Berserk source code)
+	// but with a slightly different condition
 	// http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
 	// https://github.com/jhonnold/berserk/blob/dd1678c278412898561d40a31a7bd08d49565636/src/search.c#L379
 	if (depth >= 4 && (!ttHit || tte.flags == HFNONE) && !excludedMove)
