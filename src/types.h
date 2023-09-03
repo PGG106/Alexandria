8720f6c9c4a3f26d37d7cb77f5524198c252f5c1
PGG106 <72303893+PGG106@users.noreply.github.com>
2023-09-03 21:24:16 +0200

    increase TTKey size

--- src/types.h
+++ src/types.h
@@ -7,7 +7,7 @@
 // define bitboard data type
 using Bitboard = uint64_t;
 // define poskey data type
-using TTKey = uint16_t;
+using TTKey = uint32_t;
 // define poskey data type
 using ZobristKey = uint64_t;
 
