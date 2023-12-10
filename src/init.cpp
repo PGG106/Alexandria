b688187982cef4f2692807998f13bab27c8d2fcd
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-12-10 10:04:52 +0800

    Bench 8440042

--- src/init.cpp
+++ src/init.cpp
@@ -244,8 +244,8 @@ void InitReductions() {
 
     for (int i = 1; i < MAXDEPTH; i++) {
         for (int j = 1; j < MAXDEPTH; j++) {
-            reductions[0][i][j] = log(i) * log(j) / 2.00;
-            reductions[1][i][j] = log(i) * log(j) / 2.00;
+            reductions[0][i][j] = +0.00 + log(i) * log(j) / 2.00;
+            reductions[1][i][j] = +1.00 + log(i) * log(j) / 2.00;
         }
     }
 
