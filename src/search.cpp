85615853333984bcee44d9086b94b98063a02eb6
PGG106 <giuseppepagano10@gmail.com>
2023-12-26 18:49:17 +0100

    Flash out movepicker (#284)
    Bench: 9021430

--- src/search.cpp
+++ src/search.cpp
@@ -165,73 +165,6 @@ bool SEE(const S_Board* pos, const int move, const int threshold) {
     return side != Color[attacker];
 }
 
-// ScoreMoves takes a list of move as an argument and assigns a score to each move
-static inline void ScoreMoves(S_Board* pos, Search_data* sd, Search_stack* ss, S_MOVELIST* move_list, int ttMove) {
-    // Loop through all the move in the movelist
-    for (int i = 0; i < move_list->count; i++) {
-        int move = move_list->moves[i].move;
-        // If the move is from the TT (aka it's our hashmove) give it the highest score
-        if (move == ttMove) {
-            move_list->moves[i].score = INT32_MAX - 100;
-            continue;
-        }
-        // Sort promotions based on the promoted piece type
-        else if (isPromo(move)) {
-            switch (getPromotedPiecetype(move)) {
-            case QUEEN:
-                move_list->moves[i].score = queenPromotionScore;
-                break;
-            case KNIGHT:
-                move_list->moves[i].score = knightPromotionScore;
-                break;
-            case ROOK:
-                move_list->moves[i].score = badPromotionScore;
-                break;
-            case BISHOP:
-                move_list->moves[i].score = badPromotionScore;
-                break;
-            default:
-                break;
-            }
-        }
-        else if (IsCapture(move)) {
-            // Good captures get played before any move that isn't a promotion or a TT move
-            if (SEE(pos, move, -107)) {
-                int captured_piece = isEnpassant(move) ? PAWN : GetPieceType(pos->PieceOn(To(move)));
-                // Sort by most valuable victim and capthist, with LVA as tiebreaks
-                move_list->moves[i].score = mvv_lva[GetPieceType(Piece(move))][captured_piece] + GetCapthistScore(pos, sd, move) + goodCaptureScore;
-            }
-            // Bad captures are always played last, no matter how bad the history score of a move is, it will never be played after a bad capture
-            else {
-                int captured_piece = isEnpassant(move) ? PAWN : GetPieceType(pos->PieceOn(To(move)));
-                // Sort by most valuable victim and capthist, with LVA as tiebreaks
-                move_list->moves[i].score = badCaptureScore + mvv_lva[GetPieceType(Piece(move))][captured_piece] + GetCapthistScore(pos, sd, move);
-            }
-            continue;
-        }
-        // First killer move always comes after the TT move,the promotions and the good captures and before anything else
-        else if (ss->searchKillers[0] == move) {
-            move_list->moves[i].score = killerMoveScore0;
-            continue;
-        }
-        // Second killer move always comes after the first one
-        else if (ss->searchKillers[1] == move) {
-            move_list->moves[i].score = killerMoveScore1;
-            continue;
-        }
-        // After the killer moves try the Counter moves
-        else if (move == sd->CounterMoves[From((ss - 1)->move)][To((ss - 1)->move)]) {
-            move_list->moves[i].score = counterMoveScore;
-            continue;
-        }
-        // if the move isn't in any of the previous categories score it according to the history heuristic
-        else {
-            move_list->moves[i].score = GetHistoryScore(pos, sd, move, ss);
-            continue;
-        }
-    }
-}
-
 int GetBestMove(const PvTable* pvTable) {
     return pvTable->pvArray[0][0];
 }
@@ -550,36 +483,32 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, S_ThreadData* td
     }
 
 moves_loop:
-    // create move list instance
-    S_MOVELIST moveList[1];
-
-    // generate moves
-    GenerateMoves(moveList, pos);
-    // assign a score to every move based on how promising it is
-    ScoreMoves(pos, sd, ss, moveList, ttMove);
-
+    
     // old value of alpha
-    int old_alpha = alpha;
+    const int old_alpha = alpha;
     int bestScore = -MAXSCORE;
+    int move = NOMOVE;
     int bestMove = NOMOVE;
 
-    int movesSearched = 0;
+    int movesSearched = 0, totalMoves = 0;
     bool SkipQuiets = false;
+
+    Movepicker mp;
+    InitMP(&mp, pos, sd, ss, ttMove, false);
 
     // Keep track of the played quiet and noisy moves
     S_MOVELIST quietMoves, noisyMoves;
     quietMoves.count = 0, noisyMoves.count = 0;
 
     // loop over moves within a movelist
-    for (int count = 0; count < moveList->count; count++) {
-        // take the most promising move that hasn't been played yet
-        PickMove(moveList, count);
-        // get the move with the highest score in the move ordering
-        int move = moveList->moves[count].move;
+    while ((move = NextMove(&mp, false)) != NOMOVE) {
         if (move == excludedMove)
             continue;
 
-        bool isQuiet = IsQuiet(move);
+        totalMoves++;
+        ss->move = move;
+
+        const bool isQuiet = IsQuiet(move);
 
         if (isQuiet && SkipQuiets)
             continue;
@@ -778,11 +707,11 @@ moves_loop:
     // We don't have any legal moves to make in the current postion. If we are in singular search, return alpha.
     // Otherwise, if the king is in check, return a mate score, assuming closest distance to mating position.
     // If we are in neither of these 2 cases, it is stalemate.
-    if (moveList->count == 0)
+    if (totalMoves == 0 && !excludedMove) {
         return excludedMove ? alpha
-                  : inCheck ? -mate_score + ss->ply
-                  :           0;
-
+            : inCheck ? -mate_score + ss->ply
+            : 0;
+    }
     // Set the TT flag based on whether the bestScore is better than beta and if it's not based on if we changed alpha or not
     int flag = bestScore >= beta ? HFLOWER : alpha != old_alpha ? HFEXACT : HFUPPER;
 
@@ -860,31 +789,19 @@ int Quiescence(int alpha, int beta, S_ThreadData* td, Search_stack* ss) {
     // Adjust alpha based on eval
     alpha = std::max(alpha, bestScore);
 
-    // create move list instance
-    S_MOVELIST move_list[1];
+    Movepicker mp;
     // If we aren't in check we generate just the captures, otherwise we generate all the moves
-    if (!inCheck)
-        GenerateCaptures(move_list, pos);
-    else
-        GenerateMoves(move_list, pos);
-
-    // score the generated moves
-    ScoreMoves(pos, sd, ss, move_list, ttMove);
+    InitMP(&mp, pos, sd, ss, ttMove, !inCheck);
 
     int bestmove = NOMOVE;
+    int move = NOMOVE;
+    int totalMoves = 0;
 
     // loop over moves within the movelist
-    for (int count = 0; count < move_list->count; count++) {
-        PickMove(move_list, count);
-        int move = move_list->moves[count].move;
-        int moveScore = move_list->moves[count].score;
+    while ((move = NextMove(&mp, bestScore > -mate_found)) != NOMOVE) {
 
-        // See pruning
-        if (   moveScore < goodCaptureScore 
-            && bestScore > -mate_found) {
-            break;
-        }
         ss->move = move;
+        totalMoves++;
 
         // Futility pruning. If static eval is far below alpha, only search moves that win material.
         if (    bestScore > -mate_found
@@ -930,9 +847,9 @@ int Quiescence(int alpha, int beta, S_ThreadData* td, Search_stack* ss) {
     }
 
     // return mate score (assuming closest distance to mating position)
-    if (move_list->count == 0 && inCheck)
+    if (totalMoves == 0 && inCheck) {
         return -mate_score + ss->ply;
-
+    }
     // Set the TT flag based on whether the bestScore is better than beta, for qsearch we never use the exact flag
     int flag = bestScore >= beta ? HFLOWER : HFUPPER;
 
