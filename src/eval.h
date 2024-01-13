a57ec18d2d7000f5322da5edac5103e30d7f90ba
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-01-13 12:45:23 +0800

    Tidy up accumulator updates (#308)
    Simplify commit https://github.com/PGG106/Alexandria/commit/55b8575e1081622d90cbcece7c63b3427d9c2683
    
    Bench 7513941

--- src/eval.h
+++ src/eval.h
@@ -5,4 +5,4 @@ struct S_Board;
 // Returns if the position is a draw because there isn't enough mating material
 [[nodiscard]] bool MaterialDraw(const S_Board* pos);
 // position evaluation
-[[nodiscard]] int EvalPosition(const S_Board* pos);
+[[nodiscard]] int EvalPosition(S_Board* pos);
