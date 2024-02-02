5e5a70abdbf8f71640bb3d299e6dad5cd43f0a64
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-02-02 11:00:19 +0800

    Tweak parameters for reverse futility pruning (#326)
    ELO | 2.50 +- 2.01 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.90 (-2.25, 2.89) [0.00, 3.00]
    GAMES | N: 53408 W: 12670 L: 12286 D: 28452
    https://chess.swehosting.se/test/5439/
    
    Bench 6445111

--- src/search.cpp
+++ src/search.cpp
@@ -423,8 +423,8 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, S_ThreadData* td
 
     if (!pvNode) {
         // Reverse futility pruning
-        if (   depth < 9
-            && eval - 66 * (depth - improving) >= beta
+        if (   depth < 10
+            && eval - 81 * (depth - improving) >= beta
             && abs(eval) < mate_found)
             return eval;
 
