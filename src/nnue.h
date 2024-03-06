e891006a058e36b471ef3974c15ef1cf1882f896
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-03-06 13:13:22 +0800

    Bench 6080200

--- src/nnue.h
+++ src/nnue.h
@@ -7,6 +7,7 @@
 
 constexpr int INPUT_WEIGHTS = 768;
 constexpr int HIDDEN_SIZE = 1024;
+using NNUEIndices = std::pair<std::size_t, std::size_t>;
 
 struct Network {
     int16_t featureWeights[INPUT_WEIGHTS * HIDDEN_SIZE];
@@ -23,12 +24,12 @@ public:
 
     void init(const char* file);
     void add(NNUE::accumulator& board_accumulator, const int piece, const int to);
-    void update(NNUE::accumulator& board_accumulator, std::vector<std::pair<std::size_t, std::size_t>>& NNUEAdd, std::vector<std::pair<std::size_t, std::size_t>>& NNUESub);
-    void addSub(NNUE::accumulator& board_accumulator, std::size_t whiteAddIdx, std::size_t blackAddIdx, std::size_t whiteSubIdx, std::size_t blackSubIdx);
-    void addSubSub(NNUE::accumulator& board_accumulator, std::size_t whiteAddIdx, std::size_t blackAddIdx, std::size_t whiteSubIdx1, std::size_t blackSubIdx1, std::size_t whiteSubIdx2, std::size_t blackSubIdx2);
+    void update(NNUE::accumulator& board_accumulator, std::vector<NNUEIndices>& NNUEAdd, std::vector<NNUEIndices>& NNUESub);
+    void addSub(NNUE::accumulator& board_accumulator, NNUEIndices add, NNUEIndices sub);
+    void addSubSub(NNUE::accumulator& board_accumulator, NNUEIndices add, NNUEIndices sub1, NNUEIndices sub2);
     [[nodiscard]] int32_t flatten(const int16_t *acc, const int16_t *weights);
     [[nodiscard]] int32_t output(const NNUE::accumulator& board_accumulator, const bool whiteToMove);
-    [[nodiscard]] std::pair<std::size_t, std::size_t> GetIndex(const int piece, const int square);
+    [[nodiscard]] NNUEIndices GetIndex(const int piece, const int square);
     #if defined(USE_AVX2)
     [[nodiscard]] int32_t horizontal_add(const __m256i sum);
     #endif
