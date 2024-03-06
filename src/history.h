a9c2bfd9e6cc56a023c34381cb00281a75c1e1f1
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-03-06 21:28:05 +0800

    Bench 6080200

--- src/history.h
+++ src/history.h
@@ -6,15 +6,19 @@ struct Search_stack;
 struct S_MOVELIST;
 
 // Functions used to update the history heuristics
-void UpdateHistories(const S_Board* pos, Search_data* sd, Search_stack* ss, const int depth, const int bestmove, const S_MOVELIST* quiet_moves, const S_MOVELIST* noisy_moves);
-// Fuction that returns the histroy bonus
+void UpdateHistories(const S_Board* pos, Search_data* sd, Search_stack* ss, const int depth, const int bestMove, const S_MOVELIST* quietMoves, const S_MOVELIST* noisyMoves);
+// Fuction that returns the history bonus
 int history_bonus(const int depth);
 // Getters for the history heuristics
 [[nodiscard]] int GetHHScore(const S_Board* pos, const Search_data* sd, const int move);
 [[nodiscard]] int GetCHScore(const Search_data* sd, const Search_stack* ss, const int move);
+[[nodiscard]] int GetSingleCHScore(const Search_data* sd, const Search_stack* ss, const int move, const int offset);
 [[nodiscard]] int GetHistoryScore(const S_Board* pos, const Search_data* sd, const int move, const Search_stack* ss);
 [[nodiscard]] int GetCapthistScore(const S_Board* pos, const Search_data* sd, const int move);
 // Clean all the history tables
 void CleanHistories(Search_data* sd);
-// Updates the continuation history score for a single move
+// Updates history heuristics for a single move
+void updateHHScore(const S_Board* pos, Search_data* sd, int move, int bonus);
 void updateCHScore(Search_data* sd, const Search_stack* ss, const int move, const int bonus);
+void updateCapthistScore(const S_Board* pos, Search_data* sd, int move, int bonus);
+void updateSingleCHScore(Search_data* sd, const Search_stack* ss, const int move, const int bonus, const int offset);
