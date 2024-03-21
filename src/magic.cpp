e3be9c9165ad242a60408577b1f1746d1123c626
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-03-21 19:50:43 +0800

    Make relevant bits of sliders constant (#372)
    Elo | 3.49 +- 3.12 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.95 (-2.94, 2.94) [0.00, 3.00]
    Games | N: 22184 W: 5275 L: 5052 D: 11857
    Penta | [85, 2431, 5836, 2656, 84]
    https://chess.swehosting.se/test/5862/
    
    Bench 6562652

--- src/magic.cpp
+++ src/magic.cpp
@@ -7,7 +7,7 @@ Bitboard GetBishopAttacks(const int square, Bitboard occupancy) {
     // get bishop attacks assuming current board occupancy
     occupancy &= bishop_masks[square];
     occupancy *= bishop_magic_numbers[square];
-    occupancy >>= 64 - bishop_relevant_bits[square];
+    occupancy >>= 64 - bishop_relevant_bits;
 
     // return bishop attacks
     return bishop_attacks[square][occupancy];
@@ -18,7 +18,7 @@ Bitboard GetRookAttacks(const int square, Bitboard occupancy) {
     // get rook attacks assuming current board occupancy
     occupancy &= rook_masks[square];
     occupancy *= rook_magic_numbers[square];
-    occupancy >>= 64 - rook_relevant_bits[square];
+    occupancy >>= 64 - rook_relevant_bits;
 
     // return rook attacks
     return rook_attacks[square][occupancy];
