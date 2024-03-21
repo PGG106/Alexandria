c38c7a7f32c664218163da5c34355c88eff7f1d4
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-03-21 19:49:13 +0800

    Bench 6562652

--- src/types.h
+++ src/types.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 
-#define NAME "Alexandria-6.0.16"
+#define NAME "Alexandria-6.0.17"
 
 #define start_position "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
