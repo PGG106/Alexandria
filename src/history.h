cd12a51a5f2d03d7f1a2fa4072a988f321f58b0c
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-04-14 21:33:44 +0800

    Bench 6027240

--- src/history.h
+++ src/history.h
@@ -11,14 +11,14 @@ void UpdateHistories(const Position* pos, SearchData* sd, SearchStack* ss, const
 int history_bonus(const int depth);
 // Getters for the history heuristics
 [[nodiscard]] int GetHHScore(const Position* pos, const SearchData* sd, const int move);
-[[nodiscard]] int GetCHScore(const SearchData* sd, const SearchStack* ss, const int move);
-[[nodiscard]] int GetSingleCHScore(const SearchData* sd, const SearchStack* ss, const int move, const int offset);
-[[nodiscard]] int GetHistoryScore(const Position* pos, const SearchData* sd, const int move, const SearchStack* ss);
+[[nodiscard]] int GetCHScore(const SearchStack* ss, const int move);
+[[nodiscard]] int GetSingleCHScore(const SearchStack* ss, const int move, const int offset);
 [[nodiscard]] int GetCapthistScore(const Position* pos, const SearchData* sd, const int move);
+[[nodiscard]] int GetHistoryScore(const Position* pos, const SearchData* sd, const int move, const SearchStack* ss);
 // Clean all the history tables
 void CleanHistories(SearchData* sd);
 // Updates history heuristics for a single move
 void updateHHScore(const Position* pos, SearchData* sd, int move, int bonus);
-void updateCHScore(SearchData* sd, const SearchStack* ss, const int move, const int bonus);
+void updateCHScore(SearchStack* ss, const int move, const int bonus);
 void updateCapthistScore(const Position* pos, SearchData* sd, int move, int bonus);
-void updateSingleCHScore(SearchData* sd, const SearchStack* ss, const int move, const int bonus, const int offset);
+void updateSingleCHScore(SearchStack* ss, const int move, const int bonus, const int offset);
