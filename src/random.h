e1e56db99961dd0c2472bcb98607364de85871bd
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-04-27 10:28:23 +0800

    Small fix on RNG (#397)
    Bench 6373337

--- src/random.h
+++ src/random.h
@@ -5,8 +5,8 @@
 // pseudo random number state
 extern uint64_t random_state;
 
-// generate 32-bit pseudo legal numbers
-[[nodiscard]] uint64_t GetRandomU32Number();
+// generate 64-bit pseudo legal numbers
+[[nodiscard]] uint64_t GetRandomU64Number();
 
 // generate 64-bit pseudo legal numbers
 [[nodiscard]] Bitboard GetRandomBitboardNumber();
