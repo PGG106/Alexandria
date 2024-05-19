fcfc85d06a389c5ddbba7dc29a6dbe5626e4af30
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-05-19 13:04:55 +0200

    pesting Bench: 6492541

--- src/eval.cpp
+++ src/eval.cpp
@@ -23,33 +23,213 @@ bool MaterialDraw(const Position* pos) {
     return false;
 }
 
-// position evaluation
-int materialScore(Position* pos) {
-    int white_pawns = SEEValue[PAWN] * CountBits(pos->GetPieceColorBB(PAWN,WHITE));
-    int white_knights = SEEValue[KNIGHT] * CountBits(pos->GetPieceColorBB(KNIGHT,WHITE));
-    int white_bishops = SEEValue[BISHOP] * CountBits(pos->GetPieceColorBB(BISHOP,WHITE));
-    int white_rooks = SEEValue[ROOK] * CountBits(pos->GetPieceColorBB(ROOK,WHITE));
-    int white_queens = SEEValue[QUEEN] * CountBits(pos->GetPieceColorBB(QUEEN,WHITE));
+// get game phase score
+static inline int get_game_phase_score(const Position* pos)
+{
+    // white & black game phase scores
+    int white_piece_scores = 0, black_piece_scores = 0;
 
-    int white_score = white_pawns + white_knights + white_bishops + white_rooks + white_queens;
+    // loop over white pieces
+    for (int piece = WN; piece <= WQ; piece++)
+        white_piece_scores += CountBits(pos->bitboards[piece]) * material_score[opening][piece];
 
-    int black_pawns = SEEValue[PAWN] * CountBits(pos->GetPieceColorBB(PAWN,BLACK));
-    int black_knights = SEEValue[KNIGHT] * CountBits(pos->GetPieceColorBB(KNIGHT,BLACK));
-    int black_bishops = SEEValue[BISHOP] * CountBits(pos->GetPieceColorBB(BISHOP,BLACK));
-    int black_rooks = SEEValue[ROOK] * CountBits(pos->GetPieceColorBB(ROOK,BLACK));
-    int black_queens = SEEValue[QUEEN] * CountBits(pos->GetPieceColorBB(QUEEN,BLACK));
+    // loop over white pieces
+    for (int piece = BN; piece <= BQ; piece++)
+        black_piece_scores += CountBits(pos->bitboards[piece]) * -material_score[opening][piece];
 
-    int black_score = black_pawns + black_knights + black_bishops + black_rooks + black_queens;
-
-
-    return white_score - black_score;
+    // return game phase score
+    return white_piece_scores + black_piece_scores;
 }
 
-
-
 // position evaluation
-int EvalPosition(Position* pos) {
-    int score = pos->side == WHITE  ?  materialScore(pos) : -materialScore(pos);
-    score += (pos->GetPoskey() % 4);
-    return score;
+int EvalPosition(Position* pos)
+{
+
+    // get game phase score
+    int game_phase_score = get_game_phase_score(pos);
+
+    // game phase (opening, middle game, endgame)
+    int game_phase = -1;
+
+    // pick up game phase based on game phase score
+    if (game_phase_score > opening_phase_score) game_phase = opening;
+    else if (game_phase_score < endgame_phase_score) game_phase = endgame;
+    else game_phase = middlegame;
+
+    // static evaluation score
+    int score = 0, score_opening = 0, score_endgame = 0;
+
+
+    // init piece & square
+    int square;
+
+    Bitboard white_pawns = pos->bitboards[WP];
+
+    while (white_pawns) {
+        // init square
+        square = popLsb(white_pawns);
+
+        score_opening += material_score[opening][WP];
+        score_endgame += material_score[endgame][WP];
+
+        score_opening += positional_score[opening][PAWN][square];
+        score_endgame += positional_score[endgame][PAWN][square];
+    }
+
+    Bitboard white_knights = pos->bitboards[WN];
+
+    while (white_knights) {
+        // init square
+        square = popLsb(white_knights);
+
+        score_opening += material_score[opening][WN];
+        score_endgame += material_score[endgame][WN];
+
+        score_opening += positional_score[opening][KNIGHT][square];
+        score_endgame += positional_score[endgame][KNIGHT][square];
+    }
+
+    Bitboard white_bishops = pos->bitboards[WB];
+
+    while (white_bishops) {
+        // init square
+        square = popLsb(white_bishops);
+
+        score_opening += material_score[opening][WB];
+        score_endgame += material_score[endgame][WB];
+
+        score_opening += positional_score[opening][BISHOP][square];
+        score_endgame += positional_score[endgame][BISHOP][square];
+    }
+
+    Bitboard white_rooks = pos->bitboards[WR];
+
+    while (white_rooks) {
+        // init square
+        square = popLsb(white_rooks);
+
+        score_opening += material_score[opening][WR];
+        score_endgame += material_score[endgame][WR];
+
+        score_opening += positional_score[opening][ROOK][square];
+        score_endgame += positional_score[endgame][ROOK][square];
+    }
+
+    Bitboard white_queens = pos->bitboards[WQ];
+
+    while (white_queens) {
+        // init square
+        square = popLsb(white_queens);
+
+        score_opening += material_score[opening][WQ];
+        score_endgame += material_score[endgame][WQ];
+
+        score_opening += positional_score[opening][QUEEN][square];
+        score_endgame += positional_score[endgame][QUEEN][square];
+
+    }
+
+    int kingsquare = GetLsbIndex(pos->bitboards[WK]);
+
+    score_opening += material_score[opening][WK];
+    score_endgame += material_score[endgame][WK];
+
+    score_opening += positional_score[opening][KING][kingsquare];
+    score_endgame += positional_score[endgame][KING][kingsquare];
+
+    Bitboard black_pawns = pos->bitboards[BP];
+    while (black_pawns) {
+
+        // init square
+        square = popLsb(black_pawns);
+
+        score_opening += material_score[opening][BP];
+        score_endgame += material_score[endgame][BP];
+
+        score_opening -= positional_score[opening][PAWN][square^56];
+        score_endgame -= positional_score[endgame][PAWN][square^56];
+
+    }
+
+    Bitboard black_knights = pos->bitboards[BN];
+    while (black_knights) {
+
+        // init square
+        square = popLsb(black_knights);
+
+
+        score_opening += material_score[opening][BN];
+        score_endgame += material_score[endgame][BN];
+
+        score_opening -= positional_score[opening][KNIGHT][square^56];
+        score_endgame -= positional_score[endgame][KNIGHT][square^56];
+
+    }
+
+    Bitboard black_bishops = pos->bitboards[BB];
+    while (black_bishops) {
+
+        // init square
+        square = popLsb(black_bishops);
+
+        score_opening += material_score[opening][BB];
+        score_endgame += material_score[endgame][BB];
+
+        score_opening -= positional_score[opening][BISHOP][square^56];
+        score_endgame -= positional_score[endgame][BISHOP][square^56];
+
+    }
+
+    Bitboard black_rooks = pos->bitboards[BR];
+    while (black_rooks) {
+
+        // init square
+        square = popLsb(black_rooks);
+
+        score_opening += material_score[opening][BR];
+        score_endgame += material_score[endgame][BR];
+
+        score_opening -= positional_score[opening][ROOK][square^56];
+        score_endgame -= positional_score[endgame][ROOK][square^56];
+
+    }
+
+    Bitboard black_queens = pos->bitboards[BQ];
+
+    while (black_queens) {
+
+        // init square
+        square = popLsb(black_queens);
+
+        score_opening += material_score[opening][BQ];
+        score_endgame += material_score[endgame][BQ];
+
+        score_opening -= positional_score[opening][QUEEN][square^56];
+        score_endgame -= positional_score[endgame][QUEEN][square^56];
+
+    }
+
+    kingsquare = GetLsbIndex(pos->bitboards[BK]);
+
+    score_opening += material_score[opening][BK];
+    score_endgame += material_score[endgame][BK];
+
+    score_opening -= positional_score[opening][KING][kingsquare^56];
+    score_endgame -= positional_score[endgame][KING][kingsquare^56];
+
+    if (game_phase == middlegame)
+        score = (
+                        score_opening * game_phase_score +
+                        score_endgame * (opening_phase_score - game_phase_score)
+                ) / opening_phase_score;
+
+        // return pure opening score in opening
+    else if (game_phase == opening) score = score_opening;
+
+        // return pure endgame score in engame
+    else if (game_phase == endgame) score = score_endgame;
+
+
+    // return final evaluation based on side
+    return (pos->side == WHITE) ? score : -score;
 }
