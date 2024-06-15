ac8c8be15e350fb62d4e49bfe727a0d77574c251
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-06-15 17:45:28 +0200

    Bench: 5931902

--- src/time_manager.h
+++ src/time_manager.h
@@ -7,4 +7,4 @@ void Optimum(SearchInfo* info, int time, int inc);
 [[nodiscard]] bool StopEarly(const SearchInfo* info);
 [[nodiscard]] bool TimeOver(const SearchInfo* info);
 [[nodiscard]] bool NodesOver(const SearchInfo* info);
-void ScaleTm(ThreadData* td, const int bestMoveStabilityFactor);
+void ScaleTm(ThreadData* td, const int bestMoveStabilityFactor, const int evalStabilityFactor);
