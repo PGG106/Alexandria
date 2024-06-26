6a0977a4a717ec6a9f54e4d8ac90dcea4579d638
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-06-26 19:24:19 +0200

    i'm a sad individual Bench: 5597539

--- src/search.cpp
+++ src/search.cpp
@@ -657,28 +657,31 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             // Get base reduction value
             int depthReduction = reductions[isQuiet][std::min(depth, 63)][std::min(totalMoves, 63)];
 
-            // Fuck
-            if (cutNode)
-                depthReduction += 2;
+            if(isQuiet) {
 
-            // Reduce more if we are not improving
-            if (!improving)
-                depthReduction += 1;
+                // Fuck
+                if (cutNode)
+                    depthReduction += 2;
 
-            // Reduce less if the move is a refutation
-            if (move == mp.killer || move == mp.counter)
-                depthReduction -= 1;
+                // Reduce more if we are not improving
+                if (!improving)
+                    depthReduction += 1;
 
-            // Reduce less if we have been on the PV
-            if (ttPv)
-                depthReduction -= 1 + cutNode;
+                // Reduce less if the move is a refutation
+                if (move == mp.killer || move == mp.counter)
+                    depthReduction -= 1;
 
-            // Decrease the reduction for moves that give check
-            if (pos->checkers)
-                depthReduction -= 1;
+                // Reduce less if we have been on the PV
+                if (ttPv)
+                    depthReduction -= 1 + cutNode;
 
-            // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
-            depthReduction -= moveHistory / 8192;
+                // Decrease the reduction for moves that give check
+                if (pos->checkers)
+                    depthReduction -= 1;
+
+                // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
+                depthReduction -= moveHistory / 8192;
+            }
 
             // adjust the reduction so that we can't drop into Qsearch and to prevent extensions
             depthReduction = std::clamp(depthReduction, 0, newDepth - 1);
