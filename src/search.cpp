07eac9c795c4d076d61c115e4a2ff516ac6ec01b
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-06-26 20:58:56 +0200

    i'm a sad individual Bench: 4298765

--- src/search.cpp
+++ src/search.cpp
@@ -657,9 +657,6 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             // Get base reduction value
             int depthReduction = reductions[isQuiet][std::min(depth, 63)][std::min(totalMoves, 63)];
 
-            // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
-            depthReduction -= moveHistory / 8192;
-
             if(isQuiet) {
                 // Fuck
                 if (cutNode)
@@ -673,13 +670,13 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                 if (move == mp.killer || move == mp.counter)
                     depthReduction -= 1;
 
-                // Reduce less if we have been on the PV
-                if (ttPv)
-                    depthReduction -= 1 + cutNode;
 
                 // Decrease the reduction for moves that give check
                 if (pos->checkers)
                     depthReduction -= 1;
+
+                // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
+                depthReduction -= moveHistory / 8192;
             }
 
             // adjust the reduction so that we can't drop into Qsearch and to prevent extensions
