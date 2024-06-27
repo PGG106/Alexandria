e1a380b32bec85bc9f4dd6cb6392363699598330
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-06-27 05:33:08 +0200

    i'm a sad individual Bench: 6189634

--- src/search.cpp
+++ src/search.cpp
@@ -661,8 +661,9 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             if (cutNode)
                 depthReduction += 2;
 
-            // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
-            depthReduction -= moveHistory / 8192;
+            // Reduce less if we have been on the PV
+            if (ttPv)
+                depthReduction -= 1 + cutNode;
 
             if(isQuiet) {
 
@@ -678,9 +679,8 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                 if (pos->checkers)
                     depthReduction -= 1;
 
-                // Reduce less if we have been on the PV
-                if (ttPv)
-                    depthReduction -= 1 + cutNode;
+                // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
+                depthReduction -= moveHistory / 8192;
 
             }
 
