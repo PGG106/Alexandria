f59d082e10206e96cd0d86b3aa535cb9c0c7b43c
PGG106 <giuseppepagano10@gmail.com>
2024-06-29 22:07:52 +0200

    Remove most lmr tweaks for non quiet moves (#441)
    Elo   | -0.87 +- 1.88 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | -0.80 (-2.25, 2.89) [-3.00, 1.00]
    Games | N: 31608 W: 7083 L: 7162 D: 17363
    Penta | [24, 3761, 8314, 3680, 25]
    
    Bench: 6189634

--- src/search.cpp
+++ src/search.cpp
@@ -660,24 +660,26 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             if (cutNode)
                 depthReduction += 2;
 
-            // Reduce more if we are not improving
-            if (!improving)
-                depthReduction += 1;
+            if(isQuiet) {
+                // Reduce more if we are not improving
+                if (!improving)
+                    depthReduction += 1;
 
-            // Reduce less if the move is a refutation
-            if (move == mp.killer || move == mp.counter)
-                depthReduction -= 1;
+                // Reduce less if the move is a refutation
+                if (move == mp.killer || move == mp.counter)
+                    depthReduction -= 1;
 
-            // Reduce less if we have been on the PV
-            if (ttPv)
-                depthReduction -= 1 + cutNode;
+                // Decrease the reduction for moves that give check
+                if (pos->checkers)
+                    depthReduction -= 1;
 
-            // Decrease the reduction for moves that give check
-            if (pos->checkers)
-                depthReduction -= 1;
+                // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
+                depthReduction -= moveHistory / 8192;
 
-            // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
-            depthReduction -= moveHistory / 8192;
+                // Reduce less if we have been on the PV
+                if (ttPv)
+                    depthReduction -= 1 + cutNode;
+            }
 
             // adjust the reduction so that we can't drop into Qsearch and to prevent extensions
             depthReduction = std::clamp(depthReduction, 0, newDepth - 1);
