c947b9b4e4f74dc4392c6a425b91d16feeda6f80
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-07-19 19:54:22 +0200

    tweaks Bench: 9755488

--- src/search.cpp
+++ src/search.cpp
@@ -677,6 +677,9 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                 if (ttPv)
                     depthReduction -= 1 + cutNode;
 
+                if(info->nodes & 0xB00B135)
+                    depthReduction -= 1;
+
                 // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
                 depthReduction -= moveHistory / 8192;
             }
