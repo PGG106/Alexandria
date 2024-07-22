6035b688c02d4a6c095b403403d94b9eb5628e5e
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-07-22 13:16:47 +0800

    Add quiet history (#9)
    Elo | 106.37 +- 17.45 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.90 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 980 W: 517 L: 226 D: 237
    Penta | [14, 45, 175, 148, 108]
    https://chess.swehosting.se/test/7523/
    
    Bench 4684252

--- src/history.h
+++ src/history.h
@@ -7,5 +7,17 @@ struct SearchData;
 struct SearchStack;
 struct MoveList;
 
+int16_t HistoryBonus(const int depth);
+
+// Quiet history is a history table indexed by the side-to-move as well as a quiet move's [from-to].
+void UpdateQuietHistory(const Position *pos, SearchData *sd, const Move move, const int16_t bonus);
+int16_t GetQuietHistoryScore(const Position *pos, const SearchData *sd, const Move move);
+
+// Update all histories after a beta cutoff
+void UpdateAllHistories(const Position *pos, const SearchStack *ss, SearchData *sd, const int depth, const Move bestMove, const MoveList &quietMoves, const MoveList &tacticalMoves);
+
+// Get history score for a given move
+int GetHistoryScore(const Position *pos, const SearchStack *ss, const SearchData *sd, const Move move);
+
 // Clean all the history tables
 void CleanHistories(SearchData* sd);
