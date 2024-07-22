ea527865e4ac1ab53cd492c9e28ab8edd6561cfb
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-07-22 13:16:47 +0800

    Add quiet history (#9)
    Elo | 106.37 +- 17.45 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.90 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 980 W: 517 L: 226 D: 237
    Penta | [14, 45, 175, 148, 108]
    https://chess.swehosting.se/test/7523/
    
    Bench 4684252

--- src/history.cpp
+++ src/history.cpp
@@ -5,5 +5,50 @@
 #include "move.h"
 #include "search.h"
 
+int16_t HistoryBonus(const int depth) {
+    return std::min(histBonusQuadratic() * depth * depth + histBonusLinear() * depth + histBonusConst(), histBonusMax());
+}
+
+// Quiet history is a history table indexed by the side-to-move as well as a quiet move's [from-to].
+void UpdateQuietHistory(const Position *pos, SearchData *sd, const Move move, const int16_t bonus) {
+    // Scale the bonus so that the history, when updated, will be within [-quietHistMax(), quietHistMax()]
+    const int scaledBonus = bonus - sd->quietHistory[pos->side][FromTo(move)] * std::abs(bonus) / quietHistMax();
+    sd->quietHistory[pos->side][FromTo(move)] += bonus;
+}
+
+int16_t GetQuietHistoryScore(const Position *pos, const SearchData *sd, const Move move) {
+    return sd->quietHistory[pos->side][FromTo(move)];
+}
+
+// Use this function to update all quiet histories
+void UpdateAllHistories(const Position *pos, const SearchStack *ss, SearchData *sd, const int depth, const Move bestMove, const MoveList &quietMoves, const MoveList &tacticalMoves) {
+    int16_t bonus = HistoryBonus(depth);
+    if (isTactical(bestMove)) {
+        ;
+    }
+    else {
+        // Positively update the move that failed high
+        UpdateQuietHistory(pos, sd, bestMove, bonus);
+
+        // Penalise all quiets that failed to do so (they were ordered earlier but weren't as good)
+        for (int i = 0; i < quietMoves.count; ++i) {
+            Move quiet = quietMoves.moves[i].move;
+            if (bestMove == quiet) continue;
+            UpdateQuietHistory(pos, sd, quiet, -bonus);
+        }
+    }
+}
+
+int GetHistoryScore(const Position *pos, const SearchStack *ss, const SearchData *sd, const Move move) {
+    if (isTactical(move)) {
+        return 0;
+    }
+    else {
+        return GetQuietHistoryScore(pos, sd, move);
+    }
+}
+
 // Resets the history tables
-void CleanHistories(SearchData* sd) {}
+void CleanHistories(SearchData *sd) {
+    std::memset(sd->quietHistory, 0, sizeof(sd->quietHistory));
+}
