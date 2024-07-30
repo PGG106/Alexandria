a3b7aad0d9231321237e6ffa7e9a0f5e1b28396d
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-07-30 22:08:57 +0800

    Factorise quiet history based on shared from-to of moves (#18)
    Elo | 3.63 +- 2.87 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.92 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 17694 W: 4299 L: 4114 D: 9281
    Penta | [118, 2157, 4173, 2220, 179]
    https://chess.swehosting.se/test/7660/
    
    Bench 3752153

--- src/tune.h
+++ src/tune.h
@@ -109,4 +109,6 @@ TUNE_PARAM(histBonusLinear, 32, 0, 64, 2, 0.002)
 TUNE_PARAM(histBonusConst, 16, 0, 32, 1, 0.002)
 TUNE_PARAM(histBonusMax, 1200, 400, 4800, 100, 0.002)
 
-TUNE_PARAM(quietHistMax, 8192, 4096, 30000, 512, 0.002)
+TUNE_PARAM(quietHistFactoriserScale, 16, 0, 64, 2, 0.002)
+TUNE_PARAM(quietHistFactoriserMax, 2048, 2048, 20000, 300, 0.002)
+TUNE_PARAM(quietHistBucketMax, 6144, 2048, 20000, 300, 0.002)
