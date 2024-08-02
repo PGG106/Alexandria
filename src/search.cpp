75dcc342fe801f6ada71e9c69ef2cd6c32693e9c
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-08-02 15:21:56 +0200

    Remove useless branch Bench: 5821998

--- src/search.cpp
+++ src/search.cpp
@@ -497,7 +497,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             && BoardHasNonPawns(pos, pos->side)) {
 
             ss->move = NOMOVE;
-            const int R = 3 + depth / 3 + std::min((eval - beta) / 200, 3);
+            const int R = 3 + depth / 3 + std::min((eval - beta) / 200, 3 + !improving);
             ss->contHistEntry = &sd->contHist[PieceTo(NOMOVE)];
 
             MakeNullMove(pos);
