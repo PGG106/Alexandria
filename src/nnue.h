13833ec697e1570ea0cfaecd22b052cac62d548d
PGG106 <giuseppepagano10@gmail.com>
2024-08-04 15:05:18 +0200

    Remove output buffering (#450)
    passed non regression STC
    
    Elo | 1.30 +- 2.21 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.89 (-2.25, 2.89) [-3.00, 1.00]
    Games | N: 24600 W: 5893 L: 5801 D: 12906
    Penta | [91, 2782, 6460, 2878, 89]
    
    Bench: 5211058

--- src/nnue.h
+++ src/nnue.h
@@ -89,9 +89,9 @@ public:
         }
     };
 
-    void init(const char *file);
-    void accumulate(NNUE::Accumulator &board_accumulator, Position* pos);
+    static void init(const char *file);
+    static void accumulate(NNUE::Accumulator &board_accumulator, Position* pos);
     static void update(Accumulator *acc, Position* pos);
-    [[nodiscard]] int32_t ActivateFTAndAffineL1(const int16_t *us, const int16_t *them, const int16_t *weights, const int16_t bias);
-    [[nodiscard]] int32_t output(const NNUE::Accumulator &board_accumulator, const int stm, const int outputBucket);
+    [[nodiscard]] static int32_t ActivateFTAndAffineL1(const int16_t *us, const int16_t *them, const int16_t *weights, const int16_t bias);
+    [[nodiscard]] static int32_t output(const NNUE::Accumulator &board_accumulator, const int stm, const int outputBucket);
 };
