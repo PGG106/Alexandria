4c74e204b8f1bef54d4a81b8cbb148d472576cc2
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-08-23 10:01:45 +0200

    tuneify Bench: 13021897

--- src/search.cpp
+++ src/search.cpp
@@ -524,7 +524,8 @@ int Negamax(int alpha, int beta, int depth, bool predictedCutNode, ThreadData* t
 
             // Late Move Pruning. If we have searched many moves, but no beta cutoff has occurred,
             // assume that there are no better quiet moves and skip the rest.
-            if (totalMoves >= lmpMargins[std::min(depth, 63)])
+            if (   !pvNode
+                && totalMoves >= lmpMargins[std::min(depth, 63)])
                 skipQuiets = true;
 
             // Futility Pruning. At low depths, if the eval is far below alpha,
