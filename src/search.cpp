84674e275eb26d8c013da8024294d92b71cdd71b
PGG106 <giuseppepagano10@gmail.com>
2024-09-03 15:59:45 +0200

    Collapse the 3 scaling states into 1 enum (#46)
    Bench: 8286613

--- src/search.cpp
+++ src/search.cpp
@@ -585,7 +585,7 @@ int Negamax(int alpha, int beta, int depth, bool predictedCutNode, ThreadData* t
 
         // we adjust the search depth based on potential extensions
         int newDepth = depth - 1 + extension;
-        bool didLMR = false, didZWS = false, didPVS = false;
+        ScalingFactor scalingFactor = None;
 
         // Speculative prefetch of the TT entry
         TTPrefetch(keyAfter(pos, move));
@@ -626,7 +626,7 @@ int Negamax(int alpha, int beta, int depth, bool predictedCutNode, ThreadData* t
 
             // Carry out the reduced depth, zero window search
             score = -Negamax<false>(-alpha - 1, -alpha, reducedDepth, true, td, ss + 1);
-            didLMR = true;
+            scalingFactor = didLMR;
 
             // If the reduced depth search fails high, do a full depth search (but still on zero window).
             if (score > alpha && newDepth > reducedDepth) {
@@ -636,7 +636,7 @@ int Negamax(int alpha, int beta, int depth, bool predictedCutNode, ThreadData* t
                 newDepth += doDeeperSearch - doShallowerSearch;
                 if (newDepth > reducedDepth) {
                     score = -Negamax<false>(-alpha - 1, -alpha, newDepth, !predictedCutNode, td, ss + 1);
-                    didZWS = true;
+                    scalingFactor = didZWS;
                 }
             }
         }
@@ -647,18 +647,18 @@ int Negamax(int alpha, int beta, int depth, bool predictedCutNode, ThreadData* t
         // This zero window search will either confirm or deny our prediction, as the score cannot be exact (no integer lies in (-alpha - 1, -alpha))
         else if (!pvNode || totalMoves > 1) {
             score = -Negamax<false>(-alpha - 1, -alpha, newDepth, !predictedCutNode, td, ss + 1);
-            didZWS = true;
+            scalingFactor = didZWS;
         }
 
         // If this is our first move, we search with a full window.
         // Otherwise, if our zero window search fails low, this is a potential alpha raise and so we search the move fully.
         if (pvNode && (totalMoves == 1 || score > alpha)) {
             score = -Negamax<true>(-beta, -alpha, newDepth, false, td, ss + 1);
-            didPVS = true;
+            scalingFactor = didPVS;
         }
 
         // Add the move to the corresponding list, along with the data on its search
-        SearchedMove moveData(move, didLMR, didZWS, didPVS);
+        SearchedMove moveData(move, scalingFactor);
         if (isQuiet) quietMoves.add(moveData);
         else tacticalMoves.add(moveData);
 
