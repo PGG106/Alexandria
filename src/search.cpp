5007cbfa8fcee742478b9b8252ec70fd96ad1219
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-09-04 15:52:31 +0200

    title Bench: 7338537

--- src/search.cpp
+++ src/search.cpp
@@ -530,7 +530,8 @@ int Negamax(int alpha, int beta, int depth, bool predictedCutNode, ThreadData* t
         bool isQuiet    = !isTactical(move);
         int moveHistory = GetHistoryScore(pos, ss, sd, move);
 
-        if (bestScore > -MATE_FOUND) {
+        if (bestScore > -MATE_FOUND
+            && BoardHasNonPawns(pos, pos->side)) {
 
             // Late Move Pruning. If we have searched many moves, but no beta cutoff has occurred,
             // assume that there are no better quiet moves and skip the rest.
