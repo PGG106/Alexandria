650b7e553e943b88055f07547dd08edfe446c198
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-09-26 10:51:54 +0200

    idk what we are doing in here Bench: 5398641

--- src/search.cpp
+++ src/search.cpp
@@ -670,7 +670,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
         info->nodes++;
         const uint64_t nodesBeforeSearch = info->nodes;
         // Conditions to consider LMR. Calculate how much we should reduce the search depth.
-        if (totalMoves > 1 + pvNode + (evalDelta > 35) && depth >= 3 && (isQuiet || !ttPv)) {
+        if (totalMoves > 1 + pvNode + (pvNode && evalDelta > 35) && depth >= 3 && (isQuiet || !ttPv)) {
 
             // Get base reduction value
             int depthReduction = reductions[isQuiet][std::min(depth, 63)][std::min(totalMoves, 63)];
