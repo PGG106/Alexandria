7de97e77aadfe0729b0e56f9def3eaaed75a931b
PGG106 <giuseppepagano10@gmail.com>
2024-09-29 09:52:57 +0200

    Introduce Complexity, use it to tweak LMR (#472)
    Elo   | 2.14 +- 1.67 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.93 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 46374 W: 11381 L: 11096 D: 23897
    Penta | [226, 5450, 11520, 5795, 196]
    
    Elo   | 2.63 +- 1.92 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 30154 W: 6857 L: 6629 D: 16668
    Penta | [25, 3400, 7991, 3644, 17]
    
    Originality points gained: 4.
    
    Bench: 5842195

--- src/search.cpp
+++ src/search.cpp
@@ -477,6 +477,13 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
         StoreTTEntry(pos->posKey, NOMOVE, SCORE_NONE, rawEval, HFNONE, 0, pvNode, ttPv);
     }
 
+    const int complexity = [&] {
+        if (eval == 0 || rawEval == 0)
+            return 0;
+        else
+            return 100 * std::abs(eval - rawEval) / std::abs(eval);
+    }();
+
     // Improving is a very important modifier to many heuristics. It checks if our static eval has improved since our last move.
     // As we don't evaluate in check, we look for the first ply we weren't in check between 2 and 4 plies ago. If we find that
     // static eval has improved, or that we were in check both 2 and 4 plies ago, we set improving to true.
@@ -693,6 +700,9 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                 // Reduce less if we have been on the PV
                 if (ttPv)
                     depthReduction -= 1 + cutNode;
+
+                if(complexity > 50)
+                    depthReduction -= 1;
 
                 // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
                 depthReduction -= moveHistory / 8192;
