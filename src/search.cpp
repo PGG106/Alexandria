b6f3796035d511d806f351f75e096535dc651907
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-10-02 16:10:03 +0200

    turn off complexity if eval real big Bench: 5418210

--- src/search.cpp
+++ src/search.cpp
@@ -478,7 +478,8 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
     }
 
     const int complexity = [&] {
-        if (eval == 0 || rawEval == 0)
+        if (eval == 0 || rawEval == 0
+            || eval > 1000)
             return 0;
         else
             return 100 * std::abs(eval - rawEval) / std::abs(eval);
