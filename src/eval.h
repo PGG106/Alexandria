7324b7cdbffc35041889def578d20071f449b14b
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-10-10 03:15:59 +0200

    lol Bench: 5671747

--- src/eval.h
+++ src/eval.h
@@ -47,6 +47,7 @@
     int eval = EvalPositionRaw(pos);
     eval = ScaleMaterial(pos, eval);
     eval = eval * (200 - pos->get50MrCounter()) / 200;
+    eval -= pos->hisPly / 25;
     eval = (eval / 16) * 16 - 1 + (pos->posKey & 0x2);
     // Clamp eval to avoid it somehow being a mate score
     eval = std::clamp(eval, -MATE_FOUND + 1, MATE_FOUND - 1);
