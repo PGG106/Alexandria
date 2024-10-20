88ac99fd97bc33d48cbc2399b47420e736b185d0
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-10-20 20:25:34 +0200

    per title Bench: 6989427

--- src/search.cpp
+++ src/search.cpp
@@ -564,7 +564,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
     // http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
     // https://github.com/jhonnold/berserk/blob/dd1678c278412898561d40a31a7bd08d49565636/src/search.c#L379
     if (canIIR)
-        depth -= 1;
+        depth -= 1 - pvNode;
 
     // old value of alpha
     const int old_alpha = alpha;
