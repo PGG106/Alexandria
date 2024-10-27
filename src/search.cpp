cd672de962f040df098d1c015a07ea93aa5251b7
PGG106 <72303893+PGG106@users.noreply.github.com>
2024-10-27 23:10:22 +0100

    reduce more if no complex Bench: 6508490

--- src/search.cpp
+++ src/search.cpp
@@ -714,7 +714,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                     depthReduction += 2;
 
                 if(complexity < 150)
-                    depthReduction += 1;
+                    depthReduction += 1 + complexity < 100;
 
                 // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
                 depthReduction -= moveHistory / 6144;
