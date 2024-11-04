b63aa3aa465d4f87890b86cb001a4b993a13f664
PGG106 <giuseppepagano10@gmail.com>
2024-11-04 06:45:16 +0100

    Increase nmp fixed reduction to 4 (#483)
    Elo   | 3.09 +- 2.18 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.89 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 25994 W: 6399 L: 6168 D: 13427
    Penta | [78, 3032, 6558, 3239, 90]
    
    Elo   | 7.55 +- 3.62 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.89 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 8238 W: 1975 L: 1796 D: 4467
    Penta | [2, 862, 2214, 1037, 4]
    
    Bench: 7071570

--- src/search.cpp
+++ src/search.cpp
@@ -521,7 +521,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             && BoardHasNonPawns(pos, pos->side)) {
 
             ss->move = NOMOVE;
-            const int R = 3 + depth / 3 + std::min((eval - beta) / 200, 3);
+            const int R = 4 + depth / 3 + std::min((eval - beta) / 200, 3);
             ss->contHistEntry = &sd->contHist[PieceTo(NOMOVE)];
 
             MakeNullMove(pos);
--- src/types.h
+++ src/types.h
@@ -4,7 +4,7 @@
 // include the tune stuff here to give it global visibility
 #include "tune.h"
 
-#define NAME "Alexandria-7.1.0"
+#define NAME "Alexandria-7.1.1"
 
 inline int reductions[2][64][64];
 inline int lmp_margin[64][2];
