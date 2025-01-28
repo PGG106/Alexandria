6e64f131f0f1e87ded74758e94e59bdf7c0d091a
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-01-28 17:53:01 +0100

    split rfp values Bench: 18994890

--- src/history.cpp
+++ src/history.cpp
@@ -12,11 +12,11 @@ GetScore: this is simply a getter for a specific entry of the history table
 */
 
 int history_bonus(const int depth) {
-    return std::min(250 * depth + 171, historyBonusMax());
+    return std::min(300 * depth + 111, historyBonusMax());
 }
 
 int history_malus(const int depth) {
-    return std::min(300 * depth - 130, historyMalusMax());
+    return std::min(350 * depth - 147, historyMalusMax());
 }
 
 void updateHHScore(const Position* pos, SearchData* sd, const Move move, int bonus) {
