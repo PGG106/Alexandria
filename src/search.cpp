ab131e02322796b08bd3c2b5b85694b95e41e6c3
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-03-03 18:20:32 +0100

    bump Bench: 9277461

--- src/search.cpp
+++ src/search.cpp
@@ -674,6 +674,9 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                     && ss->staticEval + futilityCoeff0() + futilityCoeff1() * lmrDepth <= alpha) {
                     skipQuiets = true;
                 }
+
+                if(isQuiet && skipQuiets)
+                    continue;
             }
             int see_margin = isQuiet ? seeQuietMargin() * lmrDepth : seeNoisyMargin() * lmrDepth * lmrDepth;
             // See pruning: prune all the moves that have a SEE score that is lower than our threshold
