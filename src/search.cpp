b945ffde521cfec15d41fa7c70671b190b2d2a69
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-03-05 17:47:46 +0100

    bump Bench: 9330832

--- src/search.cpp
+++ src/search.cpp
@@ -266,7 +266,7 @@ void SearchPosition(int startDepth, int finalDepth, ThreadData* td, UciOptions* 
             }
 
             // Keep track of eval stability
-            if (score > averageScore - 10 && score < averageScore + 10) {
+            if (std::abs(score - averageScore) < 15) {
                 evalStabilityFactor = std::min(evalStabilityFactor + 1, 4);
             }
             else {
