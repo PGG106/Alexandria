f3b299fc4c3546f66aad17d43048c19602c7267d
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-03-10 12:05:48 +0100

    bump Bench: 8298597

--- src/search.cpp
+++ src/search.cpp
@@ -531,7 +531,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             && eval >= beta
             && ss->staticEval >= beta - 30 * depth + 170
             && (ss - 1)->move != NOMOVE
-            && depth >= 2
+            && depth >= 4
             && ss->ply >= td->nmpPlies
             && BoardHasNonPawns(pos, pos->side)) {
 
