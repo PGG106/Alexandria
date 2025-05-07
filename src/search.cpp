12e650e6ccae9b2b4964171aab319d5c4f81ddd8
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-05-07 15:36:10 +0200

    bump Bench: 6952014

--- src/search.cpp
+++ src/search.cpp
@@ -531,7 +531,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             && abs(eval) < MATE_FOUND
             && (ttMove == NOMOVE || isTactical(ttMove))
             && eval - futilityMargin(depth, improving, canIIR) >= beta)
-            return eval - futilityMargin(depth, improving, canIIR);
+            return eval;
 
         // Null move pruning: If our position is so good that we can give the opponent a free move and still fail high,
         // return early. At higher depth we do a reduced search with null move pruning disabled (ie verification search)
