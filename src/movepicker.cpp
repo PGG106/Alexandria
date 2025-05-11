f8bddb4a07750be5f430de45a786ce5c1f39a7a8
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-05-11 20:13:28 +0200

    pawnhist tunable set Bench: 6952737

--- src/movepicker.cpp
+++ src/movepicker.cpp
@@ -21,7 +21,7 @@ void ScoreMoves(Movepicker* mp) {
             moveList->moves[i].score = SEEValue[capturedPiece] * 16 + GetCapthistScore(pos, sd, move);
         }
         else {
-            moveList->moves[i].score = GetHistoryScore(pos, sd, move, ss, rootNode) + + GetPHScore(pos, sd, move);
+            moveList->moves[i].score = GetHistoryScore(pos, sd, move, ss, rootNode) + GetPHScore(pos, sd, move)/2;
         }
     }
 }
