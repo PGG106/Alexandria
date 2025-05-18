87e1a90537cfc78a785db36ba6b1b5dbed578712
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-05-18 19:51:27 +0200

    remove unused obj Bench: 7251138

--- src/search.cpp
+++ src/search.cpp
@@ -503,12 +503,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
         updateOppHHScore(pos, sd, move, bonus);
     }
 
-    const int complexity = [&] {
-        if (eval == 0 || rawEval == 0)
-            return 0;
-        else
-            return 100 * std::abs(eval - rawEval) / std::abs(eval);
-    }();
+    const int complexity = std::abs(eval - rawEval);
 
     // Improving is a very important modifier to many heuristics. It checks if our static eval has improved since our last move.
     // As we don't evaluate in check, we look for the first ply we weren't in check between 2 and 4 plies ago. If we find that
