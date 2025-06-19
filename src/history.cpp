9366aed8a5ddf9fe2c2a2e5bd2d582329cd281d1
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-06-19 03:46:27 +0200

    new net Bench: 6389849

--- src/history.cpp
+++ src/history.cpp
@@ -68,7 +68,10 @@ void updateCHScore(SearchStack* ss, const Move move, const int bonus) {
     // Update move score
     updateSingleCHScore(ss, move, bonus, 1);
     updateSingleCHScore(ss, move, bonus, 2);
+    updateSingleCHScore(ss, move, bonus, 3);
     updateSingleCHScore(ss, move, bonus, 4);
+    updateSingleCHScore(ss, move, bonus, 5);
+    updateSingleCHScore(ss, move, bonus, 6);
 }
 
 void updateSingleCHScore(SearchStack* ss, const Move move, const int bonus, const int offset) {
@@ -141,8 +144,11 @@ int GetRHScore(const Position *pos, const SearchData *sd, const Move move) {
 // Returns the history score of a move
 int GetCHScore(const SearchStack* ss, const Move move) {
     return   GetSingleCHScore(ss, move, 1)
-           + GetSingleCHScore(ss, move, 2)
-           + GetSingleCHScore(ss, move, 4);
+             + GetSingleCHScore(ss, move, 2)
+             + GetSingleCHScore(ss, move, 3)
+             + GetSingleCHScore(ss, move, 4)
+             + GetSingleCHScore(ss, move, 5)
+             + GetSingleCHScore(ss, move, 6);
 }
 
 int GetSingleCHScore(const SearchStack* ss, const Move move, const int offset) {
