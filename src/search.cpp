d393f83ef7021a7ac350abe6de9bf07ca0400c7f
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-08-06 08:58:12 +0200

    try it Bench: 8942782

--- src/search.cpp
+++ src/search.cpp
@@ -1014,7 +1014,7 @@ int Quiescence(int alpha, int beta, ThreadData* td, SearchStack* ss) {
                 continue;
             }
 
-            if(!SEE(pos,move,-100))
+            if(!SEE(pos,move,-50))
                 continue;
         }
         // Speculative prefetch of the TT entry
