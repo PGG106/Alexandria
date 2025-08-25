22c87fa1bb0f148989982ab5fc27e06bbd694c4b
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-08-25 12:06:54 +0200

    aaa Bench: 8794556

--- src/uci.cpp
+++ src/uci.cpp
@@ -340,7 +340,7 @@ void UciLoop(int argc, char** argv) {
             std::cout << "id author Zuppa, CJ and Contributors\n";
             std::cout << "option name Hash type spin default 16 min 1 max 262144 \n";
             std::cout << "option name Threads type spin default 1 min 1 max 256 \n";
-            std::cout << "option name UCI_Minimal type check default false \n";
+            std::cout << "option name Minimal type check default false \n";
 #ifdef TUNE
             // spsa info dump
             for (const auto &param: tunables()) {
