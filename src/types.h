ea67f443f2654491df42c34353f88d603b233e33
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-08-29 07:14:59 +0200

    stuff Bench: 8605734

--- src/types.h
+++ src/types.h
@@ -4,7 +4,7 @@
 // include the tune stuff here to give it global visibility
 #include "tune.h"
 
-#define NAME "Alexandria-8.1.2"
+#define NAME "Alexandria-8.1.4"
 
 inline int reductions[2][64][64];
 inline int lmp_margin[64][2];
