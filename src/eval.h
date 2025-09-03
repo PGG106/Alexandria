d3bee059ae20694216b78cd5c344b0ebf088c09a
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-09-03 18:02:44 +0200

    bump version Bench: 8940980

--- src/eval.h
+++ src/eval.h
@@ -5,22 +5,6 @@
 
 // if we don't have enough material to mate consider the position a draw
 [[nodiscard]] inline bool MaterialDraw(const Position* pos) {
-    // If we only have kings on the board then it's a draw
-    if (pos->PieceCount() == 2)
-        return true;
-    // KN v K, KB v K
-    else if (pos->PieceCount() == 3 && ((CountBits(GetPieceBB(pos, KNIGHT)) == 1) || (CountBits(GetPieceBB(pos, BISHOP)) == 1)))
-        return true;
-    // If we have 4 pieces on the board
-    else if (pos->PieceCount() == 4) {
-        // KNN v K, KN v KN
-        if ((CountBits(GetPieceBB(pos, KNIGHT)) == 2))
-            return true;
-        // KB v KB
-        else if (((CountBits(GetPieceBB(pos, BISHOP)) == 2)) && CountBits(pos->GetPieceColorBB(BISHOP, WHITE)) == 1)
-            return true;
-    }
-
     return false;
 }
 
