a47c67770e26f6690d707a3dd2010438c640f213
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-09-08 00:01:27 +0200

    bla bla Bench: 9269329

--- src/search.cpp
+++ src/search.cpp
@@ -650,7 +650,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
     // IIR by Ed Schroder (That i find out about in Berserk source code)
     // http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
     // https://github.com/jhonnold/berserk/blob/dd1678c278412898561d40a31a7bd08d49565636/src/search.c#L379
-    if ( depth >= 4 + 3 * cutNode && (pvNode || cutNode))
+    if ( depth >= 4 + 3 * cutNode && (pvNode || cutNode) && ttMove == NOMOVE)
         depth -= 1;
 
     // old value of alpha
