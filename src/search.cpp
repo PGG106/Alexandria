d71cd235c0c75ab469e8352ae6f1158c0ea0888c
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-09-14 01:29:03 +0200

    Bench: 10879392

--- src/search.cpp
+++ src/search.cpp
@@ -737,8 +737,8 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
 
                 if (singularScore < singularBeta) {
                     extension = 1;
-                    const int doubleMargin = 10 + 280 * pvNode;
-                    const int tripleMargin = 75 + 320 * pvNode;
+                    const int doubleMargin = 2 + 277 * pvNode;
+                    const int tripleMargin = 67 + 315 * pvNode;
                     if (singularScore < singularBeta - doubleMargin) {
                         extension = 2 + (!isTactical(ttMove) && singularScore < singularBeta - tripleMargin);
                         depth += depth < 10;
