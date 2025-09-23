a1935571fb61e19a9e9dd6778d464d70e27cb9e9
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-09-23 10:40:46 +0200

    pawnhist again part 5 Bench: 9458072

--- src/history.h
+++ src/history.h
@@ -9,8 +9,8 @@ struct SearchStack;
 struct MoveList;
 
 constexpr int HH_MAX = 8192;
-constexpr int PH_MAX = 8192;
-constexpr int PH_SIZE = 1024;
+constexpr int PH_MAX = 1024;
+constexpr int PH_SIZE = 512;
 constexpr int RH_MAX = 8192;
 constexpr int CH_MAX = 16384;
 constexpr int CAPTHIST_MAX = 16384;
