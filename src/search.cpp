4a6c372c4b30a77d675dde0ae5b28e41a307e4a7
PGG106 <72303893+PGG106@users.noreply.github.com>
2025-10-07 09:53:07 +0200

    oopsie Bench: 8996603

--- src/search.cpp
+++ src/search.cpp
@@ -471,7 +471,8 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
         && pos->get50MrCounter() < 90
         && (ttBound & (ttScore >= beta ? HFLOWER : HFUPPER))) {
         if (ttMove && ttScore >= beta && (ss-1)->moveCount < 4 && isQuiet((ss-1)->move)) {
-            updateCHScore((ss-1), ttMove, -100);
+            if ((ss-1)->move != NOMOVE)
+            updateCHScore((ss-1), (ss-1)->move, -100);
         }
         return ttScore;
     }
