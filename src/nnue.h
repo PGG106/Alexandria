714c59d49552e995ba44886fcbf2222134647f24
PGG106 <72303893+PGG106@users.noreply.github.com>
2026-01-03 17:43:58 +0100

    ad nnz struct Bench: 9870769

--- src/nnue.h
+++ src/nnue.h
@@ -5,6 +5,9 @@
 #include <vector>
 #include <cassert>
 #include <cmath>
+#include <cstring>
+
+#include "bitboard.h"
 #include "simd.h"
 #include "types.h"
 
@@ -89,6 +92,81 @@ struct Network {
     alignas(64) float   L3Weights[OUTPUT_BUCKETS][L3_SIZE];
     alignas(64) float   L3Biases [OUTPUT_BUCKETS];
 };
+
+struct NNZData {
+    uint64_t data[L1_SIZE / 2]{};
+    int indexOrder[L1_SIZE / 2];
+
+    NNZData() {
+        // First init the index order to default
+        for (int i = 0; i < L1_SIZE / 2; ++i) indexOrder[i] = i;
+    };
+
+    inline void update(auto *outputs) {
+        // Update based on if the neuron is activated
+        for (int i = 0; i < L1_SIZE; ++i) data[i % (L1_SIZE / 2)] += bool(outputs[i]);
+    };
+
+    inline void permute(QuantisedNetwork &quantisedNet) {
+        // Sort the order based on sparsity (i.e., how little it is activated)
+        std::stable_sort(indexOrder, indexOrder + L1_SIZE / 2, [&](const int &a, const int &b) { return data[a] < data[b]; });
+
+        std::memcpy(FTWeightsCopy, quantisedNet.FTWeights, sizeof(quantisedNet.FTWeights));
+        std::memcpy(FTBiasesCopy , quantisedNet.FTBiases , sizeof(quantisedNet.FTBiases ));
+        std::memcpy(L1WeightsCopy, quantisedNet.L1Weights, sizeof(quantisedNet.L1Weights));
+
+        for (int i = 0; i < L1_SIZE / 2; ++i) {
+            int oldIndex1 = indexOrder[i];
+            int newIndex1 = i;
+            int oldIndex2 = oldIndex1 + L1_SIZE / 2;
+            int newIndex2 = newIndex1 + L1_SIZE / 2;
+
+            // Permute FT weights
+            for (int j = 0; j < NUM_INPUTS; ++j) {
+                quantisedNet.FTWeights[j * L1_SIZE + newIndex1] = FTWeightsCopy[j * L1_SIZE + oldIndex1];
+                quantisedNet.FTWeights[j * L1_SIZE + newIndex2] = FTWeightsCopy[j * L1_SIZE + oldIndex2];
+            }
+
+            // Permute FT Biases
+            quantisedNet.FTBiases[newIndex1] = FTBiasesCopy[oldIndex1];
+            quantisedNet.FTBiases[newIndex2] = FTBiasesCopy[oldIndex2];
+
+            // Permute L1 weights
+            for (int j = 0; j < OUTPUT_BUCKETS; ++j) {
+                for (int k = 0; k < L2_SIZE; ++k) {
+                    quantisedNet.L1Weights[newIndex1][j][k] = L1WeightsCopy[oldIndex1][j][k];
+                    quantisedNet.L1Weights[newIndex2][j][k] = L1WeightsCopy[oldIndex2][j][k];
+                }
+            }
+        }
+    };
+
+    private:
+    int16_t FTWeightsCopy[NUM_INPUTS * L1_SIZE];
+    int16_t FTBiasesCopy [L1_SIZE];
+    int8_t  L1WeightsCopy[L1_SIZE][OUTPUT_BUCKETS][L2_SIZE];
+};
+
+// NNZTable stores all the possible 8-bit combinations, active indices and active indices count
+struct NNZEntry {
+    uint16_t indices[8];
+    int      count;
+};
+
+struct NNZTable {
+    NNZEntry table[256];
+
+    NNZTable() {
+        for (uint16_t i = 0; i <= 255; ++i) {
+            this->table[i].count = CountBits(i);
+            Bitboard j = i;
+            uint16_t k = 0;
+            while (j)
+                this->table[i].indices[k++] = popLsb(j);
+        }
+    };
+};
+
 
 extern const Network * net;
 struct Position;
