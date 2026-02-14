d046320fb9193d1ecebf5bf9cef40f1e80843c41
PGG106 <giuseppepagano10@gmail.com>
2026-02-14 11:45:08 +0100

    New net (Dual activation) (#600)
    Elo   | 1.89 +- 1.51 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.91 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 55118 W: 14014 L: 13714 D: 27390
    Penta | [204, 6557, 13725, 6881, 192]
    
    Elo   | 2.24 +- 1.71 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.89 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 38084 W: 9489 L: 9244 D: 19351
    Penta | [27, 4353, 10032, 4608, 22]
    
    Bench: 9260898

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -65,7 +65,7 @@ void load_unquantize_andquant() {
         }
 
         // Quantise L2 Weights
-        for (int i = 0; i < L2_SIZE; ++i)
+        for (int i = 0; i < EFFECTIVE_L2_SIZE; ++i)
             for (int j = 0; j < L3_SIZE; ++j)
                 quantisedNet.L2Weights[i][bucket][j] = unquantisedNet.L2Weights[i][bucket][j];
 
@@ -81,7 +81,7 @@ void load_unquantize_andquant() {
         quantisedNet.L3Biases[bucket] = unquantisedNet.L3Biases[bucket];
     }
 
-    std::ofstream out{"quatnnet.net", std::ios::binary};
+    std::ofstream out{"nn.net", std::ios::binary};
     out.write(reinterpret_cast<const char *>(&quantisedNet), sizeof(QuantisedNetwork));
     exit(12);
 }
@@ -238,11 +238,17 @@ void NNUE::propagateL1(const uint8_t *inputs, [[maybe_unused]] uint16_t *nnzIndi
         const vps32 biasVec = vec_load_ps(&biases[i * L2_CHUNK_SIZE]);
         const vps32 sumMul = vec_set1_ps(L1_MUL);
         const vps32 sumPs = vec_mul_add_ps(vec_cvtepi32_ps(sums[i]), sumMul, biasVec);
+
         const vps32 Zero = vec_zero_ps();
         const vps32 One = vec_set1_ps(1.0f);
+        // linear
         const vps32 clipped = vec_min_ps(vec_max_ps(sumPs, Zero), One);
-        const vps32 squared = vec_mul_ps(clipped, clipped);
-        vec_store_ps(&output[i * L2_CHUNK_SIZE], squared);
+        // squared
+        const vps32 squared = vec_mul_ps(sumPs, sumPs);
+        const vps32 squared_clipped = vec_min_ps(vec_max_ps(squared, Zero), One);
+        // it's storing time
+        vec_store_ps(&output[i * L2_CHUNK_SIZE], clipped);
+        vec_store_ps(&output[L2_SIZE + i * L2_CHUNK_SIZE], squared_clipped);
     }
 #else
     int sums[L2_SIZE] = {};
@@ -254,9 +260,12 @@ void NNUE::propagateL1(const uint8_t *inputs, [[maybe_unused]] uint16_t *nnzIndi
 
     for (int i = 0; i < L2_SIZE; ++i) {
         // Convert into floats and activate L1
-        const float clipped = std::clamp(float(sums[i]) * L1_MUL + biases[i], 0.0f, 1.0f);
-        const float squared = clipped * clipped;
+        const float z = float(sums[i]) * L1_MUL + biases[i];
+        // Dual activation: produce 2 L1 outputs for each input by applying different activations
+        const float squared = std::clamp(z * z, 0.0f, 1.0f);
+        const float linear =  std::clamp(z, 0.0f, 1.0f);
         output[i] = squared;
+        output[i+ L2_SIZE] = linear;
     }
 #endif
 }
@@ -269,7 +278,7 @@ void NNUE::propagateL2(const float *inputs, const float *weights, const float *b
     for (int i = 0; i < L3_SIZE / L3_CHUNK_SIZE; ++i)
         sumVecs[i] = vec_load_ps(&biases[i * L3_CHUNK_SIZE]);
 
-    for (int i = 0; i < L2_SIZE; ++i) {
+    for (int i = 0; i < EFFECTIVE_L2_SIZE; ++i) {
         const vps32 inputVec = vec_set1_ps(inputs[i]);
         const vps32 *weight = reinterpret_cast<const vps32 *>(&weights[i * L3_SIZE]);
         for (int j = 0; j < L3_SIZE / L3_CHUNK_SIZE; ++j)
@@ -345,7 +354,7 @@ int NNUE::output(Position *pos, NNUE::FinnyTable *FinnyPointer) {
     const int pieceCount = pos->PieceCount();
     const int outputBucket = std::min((63 - pieceCount) * (32 - pieceCount) / 225, 7);
     alignas (64) uint8_t FTOutputs[L1_SIZE];
-    alignas (64) float L1Outputs[L2_SIZE];
+    alignas (64) float L1Outputs[EFFECTIVE_L2_SIZE];
     alignas (64) float L2Outputs[L3_SIZE];
     float L3Output;
 
