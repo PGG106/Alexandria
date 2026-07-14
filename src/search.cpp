1d46d2b17a455d608ec28dd37fc6965b7b72cca0
gpagano <g.pagano0@cineca.it>
2026-07-14 09:35:26 +0200

    Reduce less when TT entry has sufficient depth (isolated)\n\nBench: 9850674

--- src/search.cpp
+++ src/search.cpp
@@ -788,6 +788,9 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
             // Get base reduction value
             int depthReduction = reductions[isQuiet][std::min(depth, 63)][std::min(totalMoves, 63)];
 
+            // Reduce less if we have a TT entry with sufficient depth
+            depthReduction -= (ttHit && ttDepth >= depth);
+
             if (isQuiet) {
                 // Fuck
                 if (cutNode)
